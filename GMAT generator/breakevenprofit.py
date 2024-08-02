8656e7d82af1db8d0f0fb36ca8f5200121ef464b
Tom Anderson <120120401+SnakeMistake@users.noreply.github.com>
2024-08-02 15:03:32 -0400

    Delete GMAT generator directory
    Moved generator files to local branch only

--- GMAT generator/breakevenprofit.py
+++
@@ -1,3 +0,0 @@
-question_template = "Company Q plans to make a new product next year and sell each unit of this new product at a selling price of $2. The variable costs per unit in each production run are estimated to be 40% of the selling price, and the fixed costs for each production run are estimated to be $5,040. Based on these estimated costs, how many units of the new product will Company Q need to make and sell in order for their revenue to equal their total costs for each production run?"
-correct_answer = 4200
-incorrect_answers = [3150, 2520, 2100, 1800]
--- GMAT generator/quadraticword.py
+++
@@ -1,9 +0,0 @@
-from random import randint, choice, shuffle, sample
-
-question_template = "The product of two negative numbers is 160. If the lesser of the two numbers is 4 less than twice the greater, what is the greater number?"
-
-source = "PS 151 (OG 2022)"
-corrct_answer = -8
-wrong_answers = [-20, -16,-10,-4]
-
-
